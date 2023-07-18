4859875baa770d9ba0080b187df6917299492397
George <beruashvili2004@inbox.ru>
2023-07-19 01:03:55 +0400

    maria.

--- index.js
+++ index.js
@@ -98,3 +98,23 @@ const dotJump = () => {
 dotJump();
 
 // End - IMAGE CAROUSEL SCRIPT 
+
+
+
+
+
+
+var tablinks = document.getElementsByClassName("tab-links")
+var tabitems = document.getElementsByClassName("tab-items")
+
+
+function opentab(tabname) {
+  for(tablink of tablinks) {
+    tablink.classList.remove("active-link")
+  }
+  for(tabitem of tabitems) {
+    tabitem.classList.remove("active-tab")
+  }
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab")
+}
