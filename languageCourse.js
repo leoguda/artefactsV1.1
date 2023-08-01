56515bc86e22834be8c823f506d913f06972b88a
leoguda <pitodipli002@gmail.com>
2023-08-01 23:14:34 +0400

    created laguage page

---
+++ languageCourse.js
@@ -0,0 +1,76 @@
+// 2. This code loads the IFrame Player API code asynchronously.
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+
+function onYouTubeIframeAPIReady() {
+  console.log("YouTube is Ready!")
+}
+
+// 4. The API will call this function when the video player is ready.
+function onPlayerReady(event) {
+  event.target.playVideo();
+}
+
+// 5. The API calls this function when the player's state changes.
+//    The function indicates that when playing a video (state=1),
+//    the player should play for six seconds and then stop.
+
+function stopVideo() {
+  player.stopVideo();
+}
+
+let player;
+function playThisVideo(vidid){
+    if(player) {
+        player.destroy();
+    }
+
+    player = new YT.Player('player', {
+        height: '720',
+        width: '1280',
+        videoId: vidid,
+        events: {
+        'onReady': onPlayerReady,
+        'onStateChange': onPlayerStateChange
+        }
+  });
+  var done = false;
+function onPlayerStateChange(event) {
+  if (event.data == YT.PlayerState.PLAYING && !done) {
+    setTimeout(stopVideo, 0);
+    done = true;
+  }
+}
+}
+
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+const currentVideos = document.getElementsByClassName("buttons");
+
+function showCurrentVideo(){
+    for(currentVideo of currentVideos){
+        currentVideo.classList.remove("hoverColor")
+    }
+    event.currentTarget.classList.add("hoverColor");
+}
+
