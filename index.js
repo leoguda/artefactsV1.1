b3dc65d0bd271909793c74727c9ef1a19e82192e
leoguda <pitodipli002@gmail.com>
2023-08-01 14:41:24 +0400

    rearranged code
     - renamed projectGoal html, css and js into svanuri html, css and js + corrected links on every page so  a teg everywhere works properly and goes to dedicated page.
     - created responsiveNavBar.js and put responsive nav bars js code inside this file. linked it to every page and now burger menu works everywhere. before this update - since burger menus js code was in index.js with other code that was solely related to the index.html, on other pages console log was giving error -> "this" variable is not defined. other than burger menus js there is authorization code in index.js that needs to be put with burger menu's code inside  responsiveNavBar.js.

--- index.js
+++ index.js
@@ -1,21 +1,3 @@
-// Start - responsive nav
-const navResponsive = document.querySelector(".nav-info-responsive");
-const open = document.querySelector(".burger-menu-open");
-const close = document.querySelector(".burger-menu-close");
-
-function openBurgerMenu(){
-  navResponsive.style.transform = 'translateX(0)';
-  event.currentTarget.style.display="none";
-  close.style.display="block";
-}
-function closeBurgerMenu(){
-  navResponsive.style.transform = 'translateX(-100%)';
-  event.currentTarget.style.display="none";
-  open.style.display="block";
-}
-// End - responsive nav
-
-
 
 // authorize popup 
 var modal = document.getElementById("myModal");
