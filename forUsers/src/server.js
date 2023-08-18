482751ba176ce03dc62d4178f4e7e077a16a215d
leoguda <pitodipli002@gmail.com>
2023-08-18 19:16:49 +0400

    created login+register

---
+++ forUsers/src/server.js
@@ -0,0 +1,49 @@
+require('dotenv').config();
+import express from "express";
+import configViewEngine from "./configs/viewEngine";
+import initWebRoutes from "./routes/web";
+import bodyParser from "body-parser";
+import cookieParser from 'cookie-parser';
+import session from "express-session";
+import connectFlash from "connect-flash";
+import passport from "passport";
+import path from "path";
+
+let app = express();
+//use images, stylesheets, javascript files - in general for all external files
+//to be linkable from any specific .ejs 
+app.use(express.static(path.join(__dirname, 'public')))
+
+//use cookie parser
+app.use(cookieParser('secret'));
+
+//config session
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 // 86400000 1 day
+    }
+}));
+
+
+// Enable body parser post data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//Config view engine
+configViewEngine(app);
+
+//Enable flash message
+app.use(connectFlash());
+
+//Config passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// init all web routes
+initWebRoutes(app);
+
+let port = process.env.PORT || 8080;
+app.listen(port, () => console.log(`Building a login system with NodeJS is running on port ${port}!`));
